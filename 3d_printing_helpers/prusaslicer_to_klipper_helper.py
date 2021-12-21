86856a25081e804dcc41e28e4eaf25c377b8c644
Przemyslaw 'Blasto' Wentrys <pwentrys@gmail.com>
2021-12-21 02:27:47 -0500

    Added automated smart multi-file checking to make life easier and going off of modify datetime on object to check if it should be used.

--- 3d_printing_helpers/prusaslicer_to_klipper_helper.py
+++ 3d_printing_helpers/prusaslicer_to_klipper_helper.py
@@ -1,21 +1,36 @@
 import os
 from pathlib import Path
 import concurrent.futures
+from datetime import datetime
 
 
 TARGET_FOLDER: str = 'files'
-TARGET_FILENAME: str = 'calibration_wheel_3h41m_0.12mm_225C_PLA_ENDER3BLTOUCH'
-TARGET_FILENAME_UPDATED: str = f'{TARGET_FILENAME}_updated'
+# TARGET_FILENAME: str = 'calibration_cube_1h48m_0.08mm_215C_PLA_ENDER3BLTOUCH'
+TARGET_FILENAME_UPDATED_STRING = '_updated'
+# TARGET_FILENAME_UPDATED: str = f'{TARGET_FILENAME}{TARGET_FILENAME_UPDATED_STRING}'
 TARGET_FILETYPE: str = 'gcode'
-TARGET_FILEPATH: str = f'{TARGET_FILENAME}.{TARGET_FILETYPE}'
-TARGET_FILEPATH_UPDATED: str = f'{TARGET_FILENAME_UPDATED}.{TARGET_FILETYPE}'
+# TARGET_FILEPATH: str = f'{TARGET_FILENAME}.{TARGET_FILETYPE}'
+# TARGET_FILEPATH_UPDATED: str = f'{TARGET_FILENAME_UPDATED}.{TARGET_FILETYPE}'
 CWD: Path = Path.cwd()
 FILES_FOLDER: Path = CWD.joinpath(TARGET_FOLDER)
+RUNTIME_FILEPATH: Path = CWD.joinpath('.last_runtime_counter')
 
 FILE_ENCODING: str = 'utf-8'
 MAX_WORKERS = os.cpu_count() * 2
 if MAX_WORKERS > 61:
     MAX_WORKERS = 61
+
+
+def update_last_runtime():
+    utc_now = datetime.utcnow().timestamp()
+    RUNTIME_FILEPATH.write_text(f'{utc_now}', encoding=FILE_ENCODING)
+
+
+def get_last_runtime() -> float:
+    if RUNTIME_FILEPATH.is_file():
+        return float(RUNTIME_FILEPATH.read_text(encoding=FILE_ENCODING))
+    else:
+        return 0.0000000
 
 
 def ensure_dir_exists(path: Path) -> bool:
@@ -95,21 +110,21 @@ def do_gcode_transform(text_dict_item: str):
         match first:
             case 'M204':
                 if len(params) > 0:
-                    param_s = get_param_by_first_char('R', params)
+                    param_s = get_param_by_first_char('S', params)
                     param_p = get_param_by_first_char('P', params)
-                    param_r = get_param_by_first_char('R', params)
+                    param_t = get_param_by_first_char('T', params)
 
                     if param_s:
                         output = f'M204 S{param_s}'
                     elif param_p:
                         output = f'M204 S{param_p}'
-                    elif param_r:
-                        output = f'M204 S{param_r}'
+                    elif param_t:
+                        output = f'M204 S{param_t}'
                     else:
                         output = ''
             case 'M201' | 'M203' | 'M205':
-                joined_params = ' '.join(params)
-                output = f'#{first} {joined_params}'
+                # joined_params = ' '.join(params)
+                output = ''  # f'#{first} {joined_params}'
             case _:
                 pass
 
@@ -143,18 +158,78 @@ def process_text(text: str) -> str:
     return text_out
 
 
+def determine_files(folder: Path, run_time: float) -> tuple:
+    """Determine which files will be looked at
+
+    Args:
+        folder (Path): Files folder.
+        run_time (float): Last runtime of this script.
+
+    Returns:
+        tuple: Tuple of files we are going to process which qualify for running script on.
+    """
+    files_list: list = []
+
+    file: Path
+    for file in folder.iterdir():
+        if file.is_file():
+            filename = file.name
+            if not filename.endswith(TARGET_FILENAME_UPDATED_STRING):
+                stat = file.stat()
+                mod_time = datetime.utcfromtimestamp(stat.st_mtime).timestamp()
+                if run_time < mod_time:
+                    files_list.append(file)
+
+    return tuple(files_list)
+
+
+def do_file_transform(file: Path) -> str:
+    """Process text in file and then write to updated file.
+
+    Args:
+        file (Path): Path of file we will be looking at
+
+    Returns:
+        str: Updated filename
+    """
+    text: str = read_text(file)
+
+    text_processed: str = process_text(text=text)
+    filename: str = f'{file.name}{TARGET_FILENAME_UPDATED_STRING}.{TARGET_FILETYPE}'
+    file.parent.joinpath(filename).write_text(text_processed, encoding=FILE_ENCODING)
+
+    return filename
+
+
 def run():
     """Run all transforms on gcode
 
     Returns:
 
     """
-    ensure_dir_exists(FILES_FOLDER)
-    file: Path = FILES_FOLDER.joinpath(TARGET_FILEPATH)
-    text: str = read_text(file)
+    start_time = datetime.utcnow()
+    print(f'Starting at: {start_time}')
 
-    text_processed: str = process_text(text=text)
-    FILES_FOLDER.joinpath(TARGET_FILEPATH_UPDATED).write_text(text_processed, encoding=FILE_ENCODING)
+    last_runtime: float = get_last_runtime()
+    update_last_runtime()
+
+    ensure_dir_exists(FILES_FOLDER)
+
+    files: tuple = determine_files(folder=FILES_FOLDER, run_time=last_runtime)
+
+    processed = []
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        for file, updated_file in zip(files, executor.map(do_file_transform, files)):
+            processed.append(f'{file} - UPDATED - {updated_file}')
+
+    end_time = datetime.utcnow()
+    run_time = end_time - start_time
+
+    print(f'Finished at: {end_time}')
+    print(f'Run Duration: {run_time}')
+
+    print('\n'.join(processed))
 
 
 if __name__ == '__main__':
